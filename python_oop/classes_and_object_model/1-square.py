55eaf3236d49efc6cd3a36f05e7f7697ec619e8a
icame <icame@example.com>
2026-05-12 22:51:18 +0200

    add

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class representing a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
