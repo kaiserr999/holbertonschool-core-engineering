27a3a39b9dab18db87fd963c59324640c98b2b6d
icame <icame@example.com>
2026-05-12 22:46:35 +0200

    add

--- python_fundamentals/exception_handling/raise_exception_msg.py
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
-"""Module to raise a NameError with a custom message."""
-
-
 def raise_exception_msg(message=""):
-    """Raises a NameError with the provided message.
-    
-    Args:
-        message: Custom message for the NameError
-        
-    Raises:
-        NameError: With the provided message
-    """
     raise NameError(message)
