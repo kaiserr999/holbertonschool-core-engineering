f2c9478d72f496230780cc4207dfd4b352a47717
icame <icame@example.com>
2026-05-15 22:31:16 +0200

    add

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+This module defines a Square class that inherits from Rectangle.
+"""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Instantiates a Square with validated size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The computed area of the square.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Returns a printable string representation of the square.
+
+        Returns:
+            str: The square description: [Square] <size>/<size>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
