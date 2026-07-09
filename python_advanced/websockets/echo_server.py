33521029e13a453464275d3b7c776537df397c86
icame <icame@example.com>
2026-07-09 10:33:02 +0200

    Fix: add content to echo_server.py

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    async with websockets.serve(handler, "0.0.0.0", 8765):
+        await asyncio.Future()
+
+
+asyncio.run(main())
