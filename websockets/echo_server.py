10176ec7f3db3ed20f06bb87a9e14ee15e40633e
icame <icame@example.com>
2026-07-09 09:28:33 +0200

    added cocket and dir

---
+++ websockets/echo_server.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()
+
+
+asyncio.run(main())
