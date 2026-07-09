08ed749a12fae32bc15b4c564549eb70e34f20d2
icame <icame@example.com>
2026-07-09 10:27:47 +0200

    Add WebSocket echo server

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -9,7 +9,7 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8765):
+    async with websockets.serve(handler, "0.0.0.0", 8765):
         await asyncio.Future()
 
 
